780d63a22f6178f08a3f55391a00af4a61f3ea11
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-02-03 12:01:36 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,38 @@
+export const item = {
+    "id": "9b754367-546e-4c7b-863c-d039fd4b07ed",
+    "profile_background": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/131.png",
+    "profile_title": "capital",
+    "about_profile": "Career ground once alone. Summer fall speech difference money six. News mission at visit six.",
+    "detail_list": [
+        {
+            "id": "459bdb26-ab15-4a11-9448-87da1f260348",
+            "topic_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/283.png",
+            "topic": "cost",
+            "right_arrow": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/117.png"
+        },
+        {
+            "id": "0f9fc3e3-2a8b-4392-9283-d1d103581c39",
+            "topic_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/186.png",
+            "topic": "herself",
+            "right_arrow": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/347.png"
+        },
+        {
+            "id": "0fd1d345-49bc-405b-8589-ff34ee7c64cd",
+            "topic_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/22.png",
+            "topic": "lawyer",
+            "right_arrow": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/78.png"
+        },
+        {
+            "id": "b12ac3cc-446e-4237-a1c3-a222c44c5b45",
+            "topic_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/346.png",
+            "topic": "theory",
+            "right_arrow": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/15.png"
+        },
+        {
+            "id": "de52bb31-a299-4ab5-9a78-19e7ba6d946f",
+            "topic_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/110.png",
+            "topic": "individual",
+            "right_arrow": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/81.png"
+        }
+    ]
+}
